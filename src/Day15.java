1ebd7f44eab971116890a254e6a188b933612f71
Rajat Saxena <saxenarajat64@yahoo.com>
2017-05-28 23:25:23 +0530

    Create Day15.java
    adding file

---
+++ src/Day15.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Day15 {
+
+	public static Node insert(Node head, int data) {
+		// Complete this method
+		if (head == null) {
+			return new Node(data);
+		}
+
+		if (head.next == null) {
+			head.next = new Node(data);
+		} else {
+			insert(head.next, data);
+		}
+
+		return head;
+	}
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		Node head = null;
+		int N = sc.nextInt();
+
+		while (N-- > 0) {
+			int ele = sc.nextInt();
+			head = insert(head, ele);
+		}
+		display(head);
+		sc.close();
+	}
+
+	public static void display(Node head) {
+		Node start = head;
+		while (start != null) {
+			System.out.print(start.data + " ");
+			start = start.next;
+		}
+	}
+
+}
+
+class Node {
+	int data;
+	Node next;
+
+	Node(int d) {
+		data = d;
+		next = null;
+	}
+}
